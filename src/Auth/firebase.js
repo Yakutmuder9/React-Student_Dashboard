0edd2aa561d7d1162d93c4dac0192a0ce9adb288
Yakut Ahmedin <93023046+Yakutmuder9@users.noreply.github.com>
2022-06-07 20:18:20 -0400

    Update firebase.js

--- src/Auth/firebase.js
+++ src/Auth/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWW6hxP_xeJ_s__z5VZZmA8skYrhBnNBs",
-  authDomain: "mydashbord-6734b.firebaseapp.com",
-  projectId: "mydashbord-6734b",
-  storageBucket: "mydashbord-6734b.appspot.com",
-  messagingSenderId: "765717553133",
-  appId: "1:765717553133:web:92aa05dca27e7455d9d1e4",
-  measurementId: "G-00CT5V0YYX",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
